fb8bae3108dbb5dabd561d2d982ea0aee183af94
chen shalev <chensha07@gmail.com>
2017-11-26 13:24:18 +0200

    changing editor font

--- app/gallery/galleryCtrl.js
+++ app/gallery/galleryCtrl.js
@@ -3,8 +3,16 @@ dogsApp.controller("galleryCtrl", function ($scope, $log, $http, $location) {
     $scope.test = "ewewew";
     // http call to get data 
     $scope.breeds=[];
-    $http.get("https://dog.ceo/api/breeds/list/all").then(function mySuccess(response) {
+    $http.get("https://dog.ceo/api/breeds/list").then(function mySuccess(response) {
         // Updating the service with the data
+        $scope.breeds=response.data.message;
+    }
+
+        for (var index = 0; index < response.data.message.length; index++) {
+            var element = array[index];
+            $scope.breeds[index]=element
+            
+        }
         $scope.breeds = response.data;
         alert(JSON.stringify(response.data));
         ;
