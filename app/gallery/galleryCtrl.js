5e41fb21511a4904d031b702052b7f33367f1b45
chen shalev <chensha07@gmail.com>
2017-11-26 13:25:16 +0200

    read list of breed to array

--- app/gallery/galleryCtrl.js
+++ app/gallery/galleryCtrl.js
@@ -6,23 +6,11 @@ dogsApp.controller("galleryCtrl", function ($scope, $log, $http, $location) {
     $http.get("https://dog.ceo/api/breeds/list").then(function mySuccess(response) {
         // Updating the service with the data
         $scope.breeds=response.data.message;
-    }
 
-        for (var index = 0; index < response.data.message.length; index++) {
-            var element = array[index];
-            $scope.breeds[index]=element
-            
-        }
-        $scope.breeds = response.data;
-        alert(JSON.stringify(response.data));
+        alert(JSON.stringify(breeds));
         ;
     }, function myError(response) {
         alert("error" + JSON.stringify(response.status));
     });
-
-
-
-
-
 
 })
